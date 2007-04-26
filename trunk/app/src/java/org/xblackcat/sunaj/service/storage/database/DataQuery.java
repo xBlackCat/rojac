8e0eddae67099a49331d47f903cfff5866a13572
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2007-04-26 19:16:01 +0000

    The JavaDoc notes has been added to the DataQuery enum

--- trunk/app/src/java/org/xblackcat/sunaj/service/storage/database/DataQuery.java
+++ trunk/app/src/java/org/xblackcat/sunaj/service/storage/database/DataQuery.java
@@ -8,14 +8,59 @@ package org.xblackcat.sunaj.service.storage.database;
 
 public enum DataQuery implements IPropertiable {
     // Object getting queries
+    /**
+     * The query should fetch the version object properties in following order:
+     * <p/>
+     * type(int), version_data(byte[64])
+     */
     GET_OBJECT_VERSION,
+    /**
+     * The query should fetch the forum group object properties in following order:
+     * <p/>
+     * id(int), name(String), sort_order(int)
+     */
     GET_OBJECT_FORUM_GROUP,
+    /**
+     * The query should fetch the forum object properties in following order:
+     * <p/>
+     * id(int), forum group(int), rated(int), inTop(int), rateLimit(int), subscribed(boolean), short name(String), name(String)
+     */
     GET_OBJECT_FORUM,
+    /**
+     * The query should fetch the new rating object properties in following order:
+     * <p/>
+     * message id(int), rate(int)
+     */
     GET_OBJECT_NEW_RATING,
+    /**
+     * The query should fetch the message object properties in following order:
+     * <p/>
+     * id (int), topic id (int), parent id (int), user id (int), forum id (int), article id (int), user title color (int), user role (int), notify on response (boolean), read (boolean), favorite (int), message date (long), update date (long), moderated date (long), subject (String), message name (String), user nick (String), user title (String), message (String)
+     */
     GET_OBJECT_MESSAGE,
+    /**
+     * The query should fetch the moderate object properties in following order:
+     * <p/>
+     * message id(int), user id(int), forum id(int), creation time(long)
+     */
     GET_OBJECT_MODERATE,
+    /**
+     * The query should fetch the new message object properties in following order:
+     * <p/>
+     * id (int), parent id(int), forum id(int), subject(String), message(String)
+     */
     GET_OBJECT_NEW_MESSAGE,
+    /**
+     * The query should fetch the user object properties in following order:
+     * <p/>
+     * id (int), name (String), nick (String), real_name (String), email (String), home_page (String), specialization (String), where_from (String), origin (String)
+     */
     GET_OBJECT_USER,
+    /**
+     * The query should fetch the rating object properties in following order:
+     * <p/>
+     * message id(int), topic id(int), user id(int), user rating(int), rate(int), rate date(long)
+     */
     GET_OBJECT_RATING
     ;
 
