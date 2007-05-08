530d137f0eb61f24f14623270542c66c404f82f5
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2007-05-08 18:35:29 +0000

    The SQLs for storing all the objects into the storage tables has been created.

--- trunk/app/src/java/org/xblackcat/sunaj/service/storage/database/DataQuery.java
+++ trunk/app/src/java/org/xblackcat/sunaj/service/storage/database/DataQuery.java
@@ -61,7 +61,63 @@ public enum DataQuery implements IPropertiable {
      * <p/>
      * message id(int), topic id(int), user id(int), user rating(int), rate(int), rate date(long)
      */
-    GET_OBJECTS_RATING
+    GET_OBJECTS_RATING,
+
+    // All insert object queries
+    /**
+     * The query for storing the version object. Set the object parameters in following order:
+     * <p/>
+     * type(int), version_data(byte[64])
+     */
+    STORE_OBJECTS_VERSION,
+    /**
+     * The query for storing the forum group object. Set the object parameters in following order:
+     * <p/>
+     * id(int), name(String), sort_order(int)
+     */
+    STORE_OBJECTS_FORUM_GROUP,
+    /**
+     * The query for storing the forum object. Set the object parameters in following order:
+     * <p/>
+     * id(int), forum group(int), rated(int), inTop(int), rateLimit(int), subscribed(boolean), short name(String), name(String)
+     */
+    STORE_OBJECTS_FORUM,
+    /**
+     * The query for storing the new rating object. Set the object parameters in following order:
+     * <p/>
+     * message id(int), rate(int)
+     */
+    STORE_OBJECTS_NEW_RATING,
+    /**
+     * The query for storing the message object. Set the object parameters in following order:
+     * <p/>
+     * id (int), topic id (int), parent id (int), user id (int), forum id (int), article id (int), user title color (int), user role (int), notify on response (boolean), read (boolean), favorite (int), message date (long), update date (long), moderated date (long), subject (String), message name (String), user nick (String), user title (String), message (String)
+     */
+    STORE_OBJECTS_MESSAGE,
+    /**
+     * The query for storing the moderate object. Set the object parameters in following order:
+     * <p/>
+     * message id(int), user id(int), forum id(int), creation time(long)
+     */
+    STORE_OBJECTS_MODERATE,
+    /**
+     * The query for storing the new message object. Set the object parameters in following order:
+     * <p/>
+     * id (int), parent id(int), forum id(int), subject(String), message(String)
+     */
+    STORE_OBJECTS_NEW_MESSAGE,
+    /**
+     * The query for storing the user object. Set the object parameters in following order:
+     * <p/>
+     * id (int), name (String), nick (String), real_name (String), email (String), home_page (String), specialization (String), where_from (String), origin (String)
+     */
+    STORE_OBJECTS_USER,
+    /**
+     * The query for storing the rating object. Set the object parameters in following order:
+     * <p/>
+     * message id(int), topic id(int), user id(int), user rating(int), rate(int), rate date(long)
+     */
+    STORE_OBJECTS_RATING
     ;
 
     private final String properyName = this.name().toLowerCase().replace('_', '.');
