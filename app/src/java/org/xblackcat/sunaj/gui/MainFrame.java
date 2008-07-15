3506922314156375e4db08ec5adaae3d1a7f3afa
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2008-07-15 21:37:48 +0000

    A new interface IConfigurable has been introduced to managementing runtime settings of application

--- app/src/java/org/xblackcat/sunaj/gui/MainFrame.java
+++ app/src/java/org/xblackcat/sunaj/gui/MainFrame.java
@@ -6,30 +6,13 @@ import org.flexdock.docking.DockingConstants;
 import org.flexdock.docking.DockingManager;
 import org.flexdock.view.View;
 import org.flexdock.view.Viewport;
-import org.xblackcat.sunaj.data.Forum;
-import org.xblackcat.sunaj.gui.model.ForumListModel;
-import org.xblackcat.sunaj.gui.model.ForumViewMode;
-import org.xblackcat.sunaj.gui.model.ForumViewModeModel;
-import org.xblackcat.sunaj.gui.render.ForumCellRenderer;
-import org.xblackcat.sunaj.gui.render.ForumListModeRenderer;
+import org.xblackcat.sunaj.gui.view.FavoritesView;
+import org.xblackcat.sunaj.gui.view.ForumsListView;
+import org.xblackcat.sunaj.gui.view.IView;
 import org.xblackcat.sunaj.i18n.Messages;
-import org.xblackcat.sunaj.service.ServiceFactory;
-import org.xblackcat.sunaj.service.options.IOptionsService;
-import org.xblackcat.sunaj.service.options.Property;
-import org.xblackcat.sunaj.service.storage.IStorage;
-import org.xblackcat.sunaj.service.storage.StorageException;
-import org.xblackcat.sunaj.util.WindowsUtils;
 
 import javax.swing.*;
-import javax.swing.event.ListDataEvent;
-import javax.swing.event.ListDataListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 /**
  * Date: 23 груд 2007
@@ -37,11 +20,11 @@ import java.awt.event.WindowEvent;
  * @author xBlackCat
  */
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements IConfigurable {
     private static final Log log = LogFactory.getLog(MainFrame.class);
 
-    // Data and models
-    private ForumListModel forumsModel = new ForumListModel();
+    private IView forumsListView = new ForumsListView();
+    private IView favoritesView = new FavoritesView();
 
     // Components
     private JTabbedPane threads;
@@ -55,46 +38,11 @@ public class MainFrame extends JFrame {
         initialize();
 
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                storeSettings();
-
-                System.exit(0);
-            }
-        });
-
-        setSize(500, 300);
-
-        loadData();
-
-        setupUserSettings();
     }
 
-    private void setupUserSettings() {
-        // TODO: Load user settings from options
-    }
-
-    private void loadData() {
-        IStorage storage = ServiceFactory.getInstance().getStorage();
-
-        try {
-            Forum[] allForums = storage.getForumAH().getAllForums();
-            forumsModel.setForums(allForums);
-        } catch (StorageException e) {
-            log.error("Can not initialize forum list", e);
-        }
-    }
-
-    private void storeSettings() {
-        IOptionsService os = ServiceFactory.getInstance().getOptionsService();
-
-        if (!os.getProperty(Property.RSDN_USER_PASSWORD_SAVE)) {
-            os.setProperty(Property.RSDN_USER_PASSWORD, null);
-        }
-
-        os.storeSettings();
+    public void loadData() {
+        forumsListView.applySettings();
+        favoritesView.applySettings();
     }
 
     private void initialize() {
@@ -122,7 +70,7 @@ public class MainFrame extends JFrame {
                 "forums_view",
                 Messages.VIEW_FORUMS_TITLE,
                 Messages.VIEW_FORUMS_TAB_TEXT,
-                createForumsView()
+                forumsListView.getComponent()
         );
 
         viewThreads.dock(viewForums, DockingConstants.WEST_REGION, 0.3f);
@@ -132,7 +80,7 @@ public class MainFrame extends JFrame {
                 "favorites_view",
                 Messages.VIEW_FAVORITES_TITLE,
                 Messages.VIEW_FAVORITES_TAB_TEXT,
-                new JScrollPane(new JTable())
+                favoritesView.getComponent()
         );
 
         viewThreads.dock(viewFavorites, DockingConstants.EAST_REGION, 0.3f);
@@ -151,57 +99,17 @@ public class MainFrame extends JFrame {
         return view;
     }
 
-    private JComponent createForumsView() {
-        JPanel forumsPane = new JPanel(new BorderLayout(2, 2));
-        JList forums = new JList(forumsModel);
-        forumsPane.add(new JScrollPane(forums));
+    public void applySettings() {
+        forumsListView.applySettings();
+        favoritesView.applySettings();
 
-        forums.setCellRenderer(new ForumCellRenderer());
-        forums.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (e.isPopupTrigger()) {
-                    // TODO: show forum popup menu.
-                } else if (e.getClickCount() > 1) {
-                    // TODO: make double-click action
-                }
-            }
-        });
-
-        JPanel buttonsPane = new JPanel(new FlowLayout(FlowLayout.RIGHT, 2, 2));
-
-        final ForumViewModeModel modeModel = new ForumViewModeModel();
-        final JComboBox viewMode = new JComboBox(modeModel);
-
-        viewMode.setEditable(false);
-        viewMode.setRenderer(new ForumListModeRenderer());
-
-        modeModel.addListDataListener(new ListDataListener() {
-            public void intervalAdded(ListDataEvent e) {
-            }
-
-            public void intervalRemoved(ListDataEvent e) {
-            }
-
-            public void contentsChanged(ListDataEvent e) {
-                if (e.getIndex0() == -1 && e.getIndex1() == -1) {
-                    ForumViewMode vm = modeModel.getSelectedItem();
-                    viewMode.setToolTipText(vm.getTooltip());
-                    forumsModel.setMode(vm);
-                }
-            }
-        });
-
-        buttonsPane.add(viewMode);
-        buttonsPane.add(WindowsUtils.setupButton("update", new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(MainFrame.this, "Test");
-            }
-        }, Messages.VIEW_FORUMS_BUTTON_UPDATE));
-
-        forumsPane.add(buttonsPane, BorderLayout.NORTH);
-
-        return forumsPane;
+        // TODO: implement
     }
 
+    public void updateSettings() {
+        forumsListView.updateSettings();
+        favoritesView.updateSettings();
+
+        // TODO: implement
+    }
 }
