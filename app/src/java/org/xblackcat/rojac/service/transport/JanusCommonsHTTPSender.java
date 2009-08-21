ade550c871158fc2dc2c4b674e2cfdb1b4a2ba47
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2009-08-21 16:16:18 +0000

    Remove from logs transport's log. Remove unfinished TracingHTTP classes.

--- app/src/java/org/xblackcat/rojac/service/transport/JanusCommonsHTTPSender.java
+++ app/src/java/org/xblackcat/rojac/service/transport/JanusCommonsHTTPSender.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.xblackcat.rojac.service.janus;
+package org.xblackcat.rojac.service.transport;
 
 import org.apache.axis.AxisFault;
 import org.apache.axis.Constants;
