61c5b1c0b7963ed08555a55026da9cffb2447716
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2009-08-23 11:26:19 +0000

    Implement db connection pool in right way

--- trunk/app/src/java/org/xblackcat/rojac/service/storage/database/connection/PooledConnectionFactoryl.java
+++ trunk/app/src/java/org/xblackcat/rojac/service/storage/database/connection/PooledConnectionFactoryl.java
@@ -1,12 +1,15 @@
 package org.xblackcat.rojac.service.storage.database.connection;
 
+import org.apache.commons.dbcp.ConnectionFactory;
 import org.apache.commons.dbcp.DriverManagerConnectionFactory;
 import org.apache.commons.dbcp.PoolableConnectionFactory;
-import org.apache.commons.pool.PoolableObjectFactory;
+import org.apache.commons.dbcp.PoolingDriver;
+import org.apache.commons.pool.ObjectPool;
 import org.apache.commons.pool.impl.GenericObjectPool;
 import org.xblackcat.rojac.service.storage.StorageInitializationException;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
@@ -16,28 +19,36 @@ import java.sql.SQLException;
  */
 
 public class PooledConnectionFactoryl extends AConnectionFactory {
-    private final PoolableObjectFactory factory;
-
     public PooledConnectionFactoryl(String configurationName) throws StorageInitializationException {
         super(configurationName);
 
-        DriverManagerConnectionFactory connectionFactory = new DriverManagerConnectionFactory(url, userName, password);
-        factory = new PoolableConnectionFactory(
+        ObjectPool connectionPool = new GenericObjectPool(null);
+
+        ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(url, userName, password);
+
+        
+         new PoolableConnectionFactory(
                 connectionFactory,
-                new GenericObjectPool(null),
+                connectionPool,
                 null,
                 "SELECT 1+1",
                 false,
                 true);
+
+        PoolingDriver driver;
+        try {
+            Class.forName("org.apache.commons.dbcp.PoolingDriver");
+            driver = (PoolingDriver) DriverManager.getDriver("jdbc:apache:commons:dbcp:");
+        } catch (ClassNotFoundException e) {
+            throw new StorageInitializationException("Can not initialize pooling driver", e);
+        } catch (SQLException e) {
+            throw new StorageInitializationException("Can not obtain pooling driver", e);
+        }
+
+        driver.registerPool("rojacdb", connectionPool);
     }
 
     public Connection getConnection() throws SQLException {
-        try {
-            return (Connection) factory.makeObject();
-        } catch (SQLException e){
-            throw e;
-        } catch (Exception e) {
-            throw new SQLException(e);
-        }
+        return DriverManager.getConnection("jdbc:apache:commons:dbcp:rojacdb");
     }
 }
