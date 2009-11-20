6083b7b32340c902dbc77a43f1103b707b0bbc1e
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2009-11-20 08:51:24 +0000

    Revise progress tracker logic. Implement base point of progress tracker controller.

--- trunk/app/src/java/org/xblackcat/rojac/service/progress/IProgressControl.java
+++ trunk/app/src/java/org/xblackcat/rojac/service/progress/IProgressControl.java
@@ -7,13 +7,68 @@ package org.xblackcat.rojac.service.progress;
  */
 
 public interface IProgressControl {
+    /**
+     * Notify listeners about job has been started.
+     */
     void fireJobStart();
 
+    /**
+     * Notify listeners about job has been started and provide a message for logging.
+     *
+     * @param logMessage message to log.
+     */
+    void fireJobStart(String logMessage);
+
+    /**
+     * Notify listeners about changing jobs progress (completeness) state.
+     *
+     * @param progress new value of progress.
+     */
     void fireJobProgressChanged(float progress);
 
+    /**
+     * Notify listeners about changing jobs progress (completeness) state and provide a message for logging.
+     *
+     * @param progress   new value of progress.
+     * @param logMessage message to log.
+     */
+    void fireJobProgressChanged(float progress, String logMessage);
+
+    /**
+     * Notify listeners about job has been stopped.
+     */
     void fireJobStop();
 
-    void addProgressListener(IProgressListener listener);
+    /**
+     * Notify listeners about job has been stopped and provide a message for logging.
+     *
+     * @param logMessage message to log.
+     */
+    void fireJobStop(String logMessage);
 
-    void removeProgressListener(IProgressListener listener);
+    /**
+     * Notify listeners about no more tasks is processed.
+     */
+    void fireIdle();
+
+    /**
+     * Notify listeners about no more tasks is processed and provide a message for logging.
+     *
+     * @param logMessage message to log.
+     */
+    void fireIdle(String logMessage);
+
+    /**
+     * Registers a new progress listener.
+     *
+     * @param l listener to register.
+     */
+    void addProgressListener(IProgressListener l);
+
+    /**
+     * Removes a progress listener.
+     *
+     * @param l listener to remove.
+     */
+    void removeProgressListener(IProgressListener l);
 }
