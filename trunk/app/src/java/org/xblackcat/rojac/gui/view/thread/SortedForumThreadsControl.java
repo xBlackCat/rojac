b91eff45e754d9e48e626cc560c4c5720c0d097e
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2010-11-18 20:04:19 +0000

    Fix reloading threads info in opened views after synchronization.

--- trunk/app/src/java/org/xblackcat/rojac/gui/view/thread/SortedForumThreadsControl.java
+++ trunk/app/src/java/org/xblackcat/rojac/gui/view/thread/SortedForumThreadsControl.java
@@ -1,16 +1,11 @@
 package org.xblackcat.rojac.gui.view.thread;
 
+import gnu.trove.TIntHashSet;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.xblackcat.rojac.data.MessageData;
-import org.xblackcat.rojac.data.ThreadStatData;
+import org.xblackcat.rojac.RojacDebugException;
 import org.xblackcat.rojac.service.ServiceFactory;
-import org.xblackcat.rojac.service.storage.IMessageAH;
 import org.xblackcat.rojac.service.storage.IStorage;
-import org.xblackcat.rojac.service.storage.StorageException;
-import org.xblackcat.rojac.util.RojacWorker;
-
-import java.util.List;
 
 /**
  * Control class of all threads of the specified forum.
@@ -48,13 +43,6 @@ public class SortedForumThreadsControl implements IThreadControl<Post> {
         model.subTreeNodesChanged(post);
     }
 
-    protected void updateState(AThreadModel<Post> model, Post post) {
-        model.pathToNodeChanged(post);
-        for (Post p : post.getChildren()) {
-            updateState(model, p);
-        }
-    }
-
     @Override
     public void markPostRead(AThreadModel<Post> model, int postId, boolean read) {
         final Post post = model.getRoot().getMessageById(postId);
@@ -65,7 +53,7 @@ public class SortedForumThreadsControl implements IThreadControl<Post> {
     }
 
     @Override
-    public void loadChildren(final AThreadModel<Post> threadModel, Post p, IItemProcessor<Post> postProcessor) {
+    public void loadThread(final AThreadModel<Post> threadModel, Post p, IItemProcessor<Post> postProcessor) {
         //  In the Sorted model only Thread object could be loaded.
 
         // Watch out for the line!
@@ -81,97 +69,37 @@ public class SortedForumThreadsControl implements IThreadControl<Post> {
         return false;
     }
 
-    private class ThreadLoader extends RojacWorker<Void, MessageData> {
-        private final Thread item;
-        private final AThreadModel<Post> threadModel;
-        private final IItemProcessor<Post> postProcessor;
-
-        public ThreadLoader(Thread item, AThreadModel<Post> threadModel, IItemProcessor<Post> postProcessor) {
-            this.item = item;
-            this.threadModel = threadModel;
-            this.postProcessor = postProcessor;
-        }
-
-        @Override
-        protected Void perform() throws Exception {
-            int itemId = item.getMessageId();
-            int forumId = item.getForumId();
-
-            MessageData[] messages;
-            try {
-                messages = storage.getMessageAH().getMessagesDataByTopicId(itemId, forumId);
-            } catch (StorageException e) {
-                log.error("Can not load message children for id = " + itemId + " (forum #" + forumId + ")", e);
-                throw e;
-            }
-
-            publish(messages);
-
-            return null;
-        }
-
-        @Override
-        protected void process(List<MessageData> chunks) {
-            item.fillThread(chunks);
-            item.setLoadingState(LoadingState.Loaded);
-
-            threadModel.nodeStructureChanged(item);
-
-            if (postProcessor != null) {
-                postProcessor.processItem(item);
+    @Override
+    public void updateModel(AThreadModel<Post> model, int[] threadIds) {
+        TIntHashSet filledThreads = new TIntHashSet();
+        for (Post post : model.getRoot().getChildren()) {
+            if (post.getThreadRoot().isFilled()) {
+                filledThreads.add(post.getThreadRoot().getMessageId());
             }
         }
-    }
 
-    private class ThreadsLoader extends RojacWorker<Void, Thread> {
-        private final int forumId;
-        private final ForumRoot rootItem;
-        private final AThreadModel<Post> model;
+        // Retain only changed filled thread to reload.
+        filledThreads.retainAll(threadIds);
 
-        public ThreadsLoader(int forumId, ForumRoot rootItem, AThreadModel<Post> model) {
-            this.forumId = forumId;
-            this.rootItem = rootItem;
-            this.model = model;
-        }
+        // Reload filled threads.
+        for (int threadId : filledThreads.toArray()) {
+            Post post = model.getRoot().getMessageById(threadId);
+            if (post != null) {
+                // Update thread children
+                Thread t = post.getThreadRoot();
 
-        @Override
-        protected Void perform() throws Exception {
-            MessageData[] threadPosts;
-            IMessageAH mAH = storage.getMessageAH();
-            try {
-                threadPosts = mAH.getTopicMessagesDataByForumId(forumId);
-            } catch (StorageException e) {
-                log.error("Can not load topics for forum #" + forumId, e);
-                throw e;
-            }
-
-            for (MessageData threadPost : threadPosts) {
-                int topicId = threadPost.getMessageId();
-
-                try {
-                    int unreadPosts = mAH.getUnreadReplaysInThread(topicId);
-                    ThreadStatData stat = mAH.getThreadStatByThreadId(topicId);
-
-                    publish(new Thread(threadPost, stat, unreadPosts, rootItem));
-                } catch (StorageException e) {
-                    log.error("Can not load statistic for topic #" + topicId, e);
+                if (t.isFilled()) {
+                    // Thread is already filled - update children
+                    loadThread(model, t, null);
+                } else {
+                    throw new RojacDebugException("Expected filled thread #" + threadId);
                 }
+            } else {
+                throw new RojacDebugException("Thread #" + threadId + " not found");
             }
-
-
-            return null;
         }
 
-        @Override
-        protected void process(List<Thread> chunks) {
-            rootItem.addThreads(chunks);
-
-            model.nodeStructureChanged(rootItem);
-        }
-
-        @Override
-        protected void done() {
-            model.markInitialized();
-        }
+        // Reload forum threads list.
+        new ThreadsLoader(model.getRoot().getForumId(), (ForumRoot) model.getRoot(), model).execute();
     }
 }
---
+++ trunk/app/src/java/org/xblackcat/rojac/gui/view/thread/ThreadsLoader.java
@@ -0,0 +1,72 @@
+package org.xblackcat.rojac.gui.view.thread;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.xblackcat.rojac.data.MessageData;
+import org.xblackcat.rojac.data.ThreadStatData;
+import org.xblackcat.rojac.service.ServiceFactory;
+import org.xblackcat.rojac.service.storage.IMessageAH;
+import org.xblackcat.rojac.service.storage.IStorage;
+import org.xblackcat.rojac.service.storage.StorageException;
+import org.xblackcat.rojac.util.RojacWorker;
+
+import java.util.List;
+
+/**
+* @author xBlackCat
+*/
+class ThreadsLoader extends RojacWorker<Void, Thread> {
+    private static final Log log = LogFactory.getLog(ThreadsLoader.class);
+
+    protected final IStorage storage = ServiceFactory.getInstance().getStorage();
+
+    private final int forumId;
+    private final ForumRoot rootItem;
+    private final AThreadModel<Post> model;
+
+    public ThreadsLoader(int forumId, ForumRoot rootItem, AThreadModel<Post> model) {
+        this.forumId = forumId;
+        this.rootItem = rootItem;
+        this.model = model;
+    }
+
+    @Override
+    protected Void perform() throws Exception {
+        MessageData[] threadPosts;
+        IMessageAH mAH = storage.getMessageAH();
+        try {
+            threadPosts = mAH.getTopicMessagesDataByForumId(forumId);
+        } catch (StorageException e) {
+            log.error("Can not load topics for forum #" + forumId, e);
+            throw e;
+        }
+
+        for (MessageData threadPost : threadPosts) {
+            int topicId = threadPost.getMessageId();
+
+            try {
+                int unreadPosts = mAH.getUnreadReplaysInThread(topicId);
+                ThreadStatData stat = mAH.getThreadStatByThreadId(topicId);
+
+                publish(new Thread(threadPost, stat, unreadPosts, rootItem));
+            } catch (StorageException e) {
+                log.error("Can not load statistic for topic #" + topicId, e);
+            }
+        }
+
+
+        return null;
+    }
+
+    @Override
+    protected void process(List<Thread> chunks) {
+        rootItem.addThread(chunks);
+
+        model.nodeStructureChanged(rootItem);
+    }
+
+    @Override
+    protected void done() {
+        model.markInitialized();
+    }
+}
