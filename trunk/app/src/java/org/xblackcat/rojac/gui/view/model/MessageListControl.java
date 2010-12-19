b6d62adeff39c2fe189a696b8951b9243994ce6c
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2010-12-19 12:41:47 +0000

    Implement showing user's post list.

--- trunk/app/src/java/org/xblackcat/rojac/gui/view/model/MessageListControl.java
+++ trunk/app/src/java/org/xblackcat/rojac/gui/view/model/MessageListControl.java
@@ -3,6 +3,11 @@ package org.xblackcat.rojac.gui.view.model;
 import org.apache.commons.lang.NotImplementedException;
 import org.xblackcat.rojac.gui.view.thread.IItemProcessor;
 import org.xblackcat.rojac.service.datahandler.IPacket;
+import org.xblackcat.rojac.service.datahandler.IPacketProcessor;
+import org.xblackcat.rojac.service.datahandler.PacketDispatcher;
+import org.xblackcat.rojac.service.datahandler.SetForumReadPacket;
+import org.xblackcat.rojac.service.datahandler.SetPostReadPacket;
+import org.xblackcat.rojac.service.datahandler.SynchronizationCompletePacket;
 import org.xblackcat.rojac.util.RojacUtils;
 
 /**
@@ -17,24 +22,29 @@ public class MessageListControl implements IModelControl<Post> {
 
     @Override
     public void markForumRead(AThreadModel<Post> model, boolean read) {
-        assert RojacUtils.checkThread(true, getClass());
-
-        // Root post is Thread object
-        model.getRoot().setDeepRead(read);
-
-        model.subTreeNodesChanged(model.getRoot());
+        throw new NotImplementedException("The method shouldn't be used.");
     }
 
     @Override
     public void markThreadRead(AThreadModel<Post> model, int threadRootId, boolean read) {
-        assert RojacUtils.checkThread(true, getClass());
-
+        throw new NotImplementedException("The method shouldn't be used.");
     }
 
     @Override
     public void markPostRead(AThreadModel<Post> model, int postId, boolean read) {
         assert RojacUtils.checkThread(true, getClass());
 
+        Post p = model.getRoot().getMessageById(postId);
+
+        if (p == null) {
+            return;
+        }
+
+        boolean postRead = p.isRead() != ReadStatus.Unread;
+        if (postRead != read) {
+            p.setRead(read);
+            model.nodeChanged(p);
+        }
     }
 
     @Override
@@ -53,6 +63,7 @@ public class MessageListControl implements IModelControl<Post> {
     public void updateModel(AThreadModel<Post> model, int... threadIds) {
         assert RojacUtils.checkThread(true, getClass());
 
+       // TODO: implement reloading list information. Make it customizable.
     }
 
     @Override
@@ -61,9 +72,34 @@ public class MessageListControl implements IModelControl<Post> {
     }
 
     @Override
-    public boolean processPacket(AThreadModel<Post> model, IPacket p) {
-        // TODO: implement
-        return false;
+    public boolean processPacket(final AThreadModel<Post> model, IPacket p) {
+        new PacketDispatcher(
+                new IPacketProcessor<SetForumReadPacket>() {
+                    @Override
+                    public void process(SetForumReadPacket p) {
+                        updateModel(model);
+                    }
+                },
+                new IPacketProcessor<SetPostReadPacket>() {
+                    @Override
+                    public void process(SetPostReadPacket p) {
+                        if (p.isRecursive()) {
+                            // Post is a root of marked thread
+                            updateModel(model);
+                        } else {
+                            // Mark as read only the post
+                            markPostRead(model, p.getPostId(), p.isRead());
+                        }
+                    }
+                },
+                new IPacketProcessor<SynchronizationCompletePacket>() {
+                    @Override
+                    public void process(SynchronizationCompletePacket p) {
+                        updateModel(model);
+                    }
+                }
+        ).dispatch(p);
+        return true;
     }
 
     @Override
--- trunk/app/src/java/org/xblackcat/rojac/gui/view/model/UserPostFavorite.java
+++ trunk/app/src/java/org/xblackcat/rojac/gui/view/model/UserPostFavorite.java
@@ -1,15 +1,21 @@
 package org.xblackcat.rojac.gui.view.model;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.xblackcat.rojac.data.MessageData;
+import org.xblackcat.rojac.data.User;
 import org.xblackcat.rojac.service.ServiceFactory;
+import org.xblackcat.rojac.service.storage.IStorage;
 import org.xblackcat.rojac.service.storage.StorageException;
 import org.xblackcat.rojac.util.RojacUtils;
+
+import java.util.Collection;
 
 /**
  * @author xBlackCat
  */
 
 class UserPostFavorite extends AnItemFavorite {
+    private final IStorage storage = ServiceFactory.getInstance().getStorage();
+
     UserPostFavorite(Integer id, String config) {
         super(id, config);
     }
@@ -21,18 +27,28 @@ class UserPostFavorite extends AnItemFavorite {
 
     @Override
     protected int loadAmount() throws StorageException {
-        return ServiceFactory.getInstance().getStorage().getMessageAH().getUnreadUserPosts(itemId);
+        return storage.getMessageAH().getUnreadUserPosts(itemId);
     }
 
     @Override
     public String loadName() throws StorageException {
-        return "User #" + itemId + " posts";
+        User user = storage.getUserAH().getUserById(itemId);
+        if (user != null) {
+            return user.getUserNick() + "'s posts";
+        } else {
+            return "User #" + itemId + " posts";
+        }
     }
 
     @Override
-    public Post getRootNode() {
+    public Post getRootNode() throws StorageException {
         assert RojacUtils.checkThread(false, getClass());
 
-        throw new NotImplementedException();
+        Collection<MessageData> messages = storage.getMessageAH().getUserPosts(itemId);
+        PostList root = new PostList();
+        root.fillList(messages);
+        root.setLoadingState(LoadingState.Loaded);
+
+        return root;
     }
 }
