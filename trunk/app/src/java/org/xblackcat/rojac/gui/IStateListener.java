126624fdb0b4be582edd8e676039b0ee29f810ca
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-04-04 14:12:02 +0000

    A change state tracking mechanizm was added.

---
+++ trunk/app/src/java/org/xblackcat/rojac/gui/IStateListener.java
@@ -0,0 +1,11 @@
+package org.xblackcat.rojac.gui;
+
+import java.util.EventListener;
+
+/**
+ * @author xBlackCat
+ */
+
+public interface IStateListener extends EventListener {
+    void stateChanged(IView source, IViewState newState);
+}
--- trunk/app/src/java/org/xblackcat/rojac/gui/view/AView.java
+++ trunk/app/src/java/org/xblackcat/rojac/gui/view/AView.java
@@ -1,6 +1,8 @@
 package org.xblackcat.rojac.gui.view;
 
+import org.xblackcat.rojac.gui.IActionListener;
 import org.xblackcat.rojac.gui.IAppControl;
+import org.xblackcat.rojac.gui.IStateListener;
 import org.xblackcat.rojac.gui.IView;
 import org.xblackcat.rojac.service.ServiceFactory;
 import org.xblackcat.rojac.service.datahandler.IPacket;
@@ -31,12 +33,6 @@ public abstract class AView extends JPanel implements IView {
 
     protected abstract IPacketProcessor<IPacket>[] getProcessors();
 
-    public void applySettings() {
-    }
-
-    public void storeSettings() {
-    }
-
     public final JComponent getComponent() {
         return this;
     }
@@ -49,5 +45,22 @@ public abstract class AView extends JPanel implements IView {
     @Override
     public final void processPacket(IPacket packet) {
         packetDispatcher.dispatch(packet);
+    }
+
+    public void addStateChangeListener(IStateListener l) {
+        listenerList.add(IStateListener.class, l);
+    }
+
+    public void removeStateChangeListener(IStateListener l) {
+        listenerList.remove(IStateListener.class, l);
+    }
+
+    protected void fireViewStateChanged() {
+        Object[] listeners = listenerList.getListenerList();
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == IStateListener.class) {
+                ((IStateListener) listeners[i + 1]).stateChanged(this, getState());
+            }
+        }
     }
 }
