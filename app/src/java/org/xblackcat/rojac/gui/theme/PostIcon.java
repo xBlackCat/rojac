2549e66786354ddf52e03db58d86d6ac574cee41
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-04-18 12:54:00 +0000

    Make partial-read icons for all the available message states.

--- app/src/java/org/xblackcat/rojac/gui/theme/PostIcon.java
+++ app/src/java/org/xblackcat/rojac/gui/theme/PostIcon.java
@@ -11,11 +11,14 @@ public enum PostIcon implements AnIcon {
     ReadPartially("message-read-partially.png"),
     Unread("message-unread.png"),
     OwnPostUnread("message-unread-my.png"),
-    OwnPost("message-read-my.png"),
-    HasResponse("message-read-has-response.png"),
+    OwnPostReadPartially("message-read-partially-my.png"),
+    OwnPostRead("message-read-my.png"),
     HasResponseUnread("message-unread-has-response.png"),
-    Response("message-read-response.png"),
-    UnreadResponse("message-unread-response.png"),
+    HasResponseReadPartially("message-read-partially-has-response.png"),
+    HasResponseRead("message-read-has-response.png"),
+    ResponseRead("message-read-response.png"),
+    ResponseReadPartially("message-read-partially-response.png"),
+    ResponseUnread("message-unread-response.png"),
     NewMessage("message-new.png");
 
     private final String filename;
