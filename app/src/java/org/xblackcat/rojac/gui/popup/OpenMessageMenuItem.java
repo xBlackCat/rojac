1479d64ecd679b960a3565fd07579625bbd8e42d
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-04-28 09:53:28 +0000

    Simplify open-message code.

--- app/src/java/org/xblackcat/rojac/gui/popup/OpenMessageMenuItem.java
+++ app/src/java/org/xblackcat/rojac/gui/popup/OpenMessageMenuItem.java
@@ -5,17 +5,21 @@ import org.xblackcat.rojac.gui.OpenMessageMethod;
 import org.xblackcat.rojac.i18n.Messages;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
-* @author xBlackCat
-*/
+ * @author xBlackCat
+ */
 class OpenMessageMenuItem extends JMenuItem {
-    public OpenMessageMenuItem(int messageId, IAppControl appControl, Messages text, OpenMessageMethod type) {
-        this(messageId, 0, appControl, text, type);
-    }
-
-    public OpenMessageMenuItem(int messageId, int rootId, IAppControl appControl, Messages text, OpenMessageMethod type) {
+    public OpenMessageMenuItem(final int messageId, final IAppControl appControl, Messages text, final OpenMessageMethod type) {
         setText(text.get());
-        addActionListener(new OpenMessageAction(messageId, rootId, appControl, type));
+        addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                appControl.openMessage(messageId, type);
+            }
+        });
     }
 }
