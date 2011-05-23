2bba7c69baed1b0dfc305e308d863887c6295f50
xblackcat <xblackcat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-05-23 03:23:37 +0000

    Mark sub-tree/whole thread was addedto toolbar (awaiting icons)

--- trunk/app/src/java/org/xblackcat/rojac/gui/popup/SetThreadReadMenuItem.java
+++ trunk/app/src/java/org/xblackcat/rojac/gui/popup/SetThreadReadMenuItem.java
@@ -1,17 +1,11 @@
 package org.xblackcat.rojac.gui.popup;
 
-import gnu.trove.set.hash.TIntHashSet;
 import org.xblackcat.rojac.data.MessageData;
-import org.xblackcat.rojac.gui.view.model.ITreeItem;
 import org.xblackcat.rojac.gui.view.model.Post;
 import org.xblackcat.rojac.gui.view.model.Thread;
+import org.xblackcat.rojac.gui.view.thread.SubTreeReadFlagSetter;
+import org.xblackcat.rojac.gui.view.thread.ThreadReadFlagSetter;
 import org.xblackcat.rojac.i18n.Messages;
-import org.xblackcat.rojac.service.ServiceFactory;
-import org.xblackcat.rojac.service.datahandler.IPacket;
-import org.xblackcat.rojac.service.datahandler.SetPostReadPacket;
-import org.xblackcat.rojac.service.datahandler.SetReadExPacket;
-import org.xblackcat.rojac.service.storage.IStorage;
-import org.xblackcat.rojac.util.RojacWorker;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -46,67 +40,5 @@ class SetThreadReadMenuItem extends JMenuItem {
                 new ThreadReadFlagSetter(read, data).execute();
             }
         });
-    }
-
-    private class ThreadReadFlagSetter extends RojacWorker<Void, Void> {
-        private final boolean read;
-        private final MessageData threadRoot;
-
-        public ThreadReadFlagSetter(boolean read, MessageData messageData) {
-            this.read = read;
-            this.threadRoot = messageData;
-        }
-
-        @Override
-        protected Void perform() throws Exception {
-            IStorage storage = ServiceFactory.getInstance().getStorage();
-            storage.getMessageAH().updateThreadReadFlag(threadRoot.getMessageId(), read);
-            return null;
-        }
-
-        @Override
-        protected void done() {
-            IPacket packet = new SetPostReadPacket(read, threadRoot.getForumId(), threadRoot.getMessageId(), true);
-            ServiceFactory.getInstance().getDataDispatcher().processPacket(packet);
-        }
-    }
-
-    private class SubTreeReadFlagSetter extends RojacWorker<Void, Void> {
-        private final boolean read;
-        private final TIntHashSet messageIds;
-
-        public SubTreeReadFlagSetter(boolean read, ITreeItem<?> post) {
-            this.read = read;
-            messageIds = new TIntHashSet();
-
-            fillMessageIds(post);
-        }
-
-        private void fillMessageIds(ITreeItem<?> post) {
-            messageIds.add(post.getMessageId());
-
-            int i = 0;
-            int childrenLength = post.getSize();
-            while (i < childrenLength) {
-                ITreeItem<?> p = post.getChild(i);
-                fillMessageIds(p);
-                i++;
-            }
-        }
-
-        @Override
-        protected Void perform() throws Exception {
-            IStorage storage = ServiceFactory.getInstance().getStorage();
-            for (int postId : messageIds.toArray()) {
-                storage.getMessageAH().updateMessageReadFlag(postId, read);
-            }
-            return null;
-        }
-
-        @Override
-        protected void done() {
-            IPacket packet = new SetReadExPacket(read, messageIds);
-            ServiceFactory.getInstance().getDataDispatcher().processPacket(packet);
-        }
     }
 }
---
+++ trunk/app/src/java/org/xblackcat/rojac/gui/view/thread/SubTreeReadFlagSetter.java
@@ -0,0 +1,51 @@
+package org.xblackcat.rojac.gui.view.thread;
+
+import gnu.trove.set.hash.TIntHashSet;
+import org.xblackcat.rojac.gui.view.model.ITreeItem;
+import org.xblackcat.rojac.service.ServiceFactory;
+import org.xblackcat.rojac.service.datahandler.IPacket;
+import org.xblackcat.rojac.service.datahandler.SetReadExPacket;
+import org.xblackcat.rojac.service.storage.IStorage;
+import org.xblackcat.rojac.util.RojacWorker;
+
+/**
+* @author xBlackCat
+*/
+public class SubTreeReadFlagSetter extends RojacWorker<Void, Void> {
+    private final boolean read;
+    private final TIntHashSet messageIds;
+
+    public SubTreeReadFlagSetter(boolean read, ITreeItem<?> post) {
+        this.read = read;
+        messageIds = new TIntHashSet();
+
+        fillMessageIds(post);
+    }
+
+    private void fillMessageIds(ITreeItem<?> post) {
+        messageIds.add(post.getMessageId());
+
+        int i = 0;
+        int childrenLength = post.getSize();
+        while (i < childrenLength) {
+            ITreeItem<?> p = post.getChild(i);
+            fillMessageIds(p);
+            i++;
+        }
+    }
+
+    @Override
+    protected Void perform() throws Exception {
+        IStorage storage = ServiceFactory.getInstance().getStorage();
+        for (int postId : messageIds.toArray()) {
+            storage.getMessageAH().updateMessageReadFlag(postId, read);
+        }
+        return null;
+    }
+
+    @Override
+    protected void done() {
+        IPacket packet = new SetReadExPacket(read, messageIds);
+        ServiceFactory.getInstance().getDataDispatcher().processPacket(packet);
+    }
+}
