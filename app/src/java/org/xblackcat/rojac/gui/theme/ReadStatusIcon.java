a021a65a0c1d6b7b0e6af5a616e19e8216f106eb
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-06-30 12:32:35 +0000

    Add read status icons for user post/reply list.

--- app/src/java/org/xblackcat/rojac/gui/theme/ReadStatusIcon.java
+++ app/src/java/org/xblackcat/rojac/gui/theme/ReadStatusIcon.java
@@ -6,20 +6,63 @@ import java.util.EnumMap;
 import java.util.Map;
 
 /**
- * @author xBlackCat Date: 29.06.11
+ * Icon factory.
+ * <p/>
+ * Date: 29.06.11
+ *
+ * @author xBlackCat
  */
 public enum ReadStatusIcon {
+    /**
+     * Single post read status icons. Common case.
+     */
     Post("message"),
+    /**
+     * Single post read status icons. Own posts case.
+     */
     OwnPost("message-my"),
+    /**
+     * Single post read status icon. Own posts with response case.
+     */
     HasResponse("message-has-response"),
+    /**
+     * Single post read status icon. Reply on own post case.
+     */
     Response("message-response"),
+    /**
+     * Common favorite read status icons.
+     */
     Favorite("favorite"),
+    /**
+     * Favorite thread read status icons.
+     */
     FavoriteThread("favorite-thread"),
+    /**
+     * Favorite user's post list read status icons.
+     */
     FavoritePostList("favorite-userposts"),
-    FavoriteResponseList("favorite-userposts"),
+    /**
+     * Reply list on favorite user's posts read status icons.
+     */
+    FavoriteResponseList("favorite-userreplies"),
+    /**
+     * Thread read status icons.
+     */
     Thread("thread"),
-    PostList("favorite-userposts"),
-    ResponseList("favorite-userposts"),;
+    /**
+     * Thread with unread replies read status icons. Note: for now it contains only "unread" icon and shouldn't be used
+     * with others statues.
+     */
+    ThreadHasResponse("thread-has-response"),
+    /**
+     * User's post list read status icons.
+     */
+    PostList("user-post"),
+    /**
+     * Reply list on user's posts read status icons.
+     */
+    ReplyList("user-reply"),
+    ;
 
     private final Map<ReadStatus, AnIcon> iconsBuffer = new EnumMap<ReadStatus, AnIcon>(ReadStatus.class);
 
