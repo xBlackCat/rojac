9e5c3eb62cba776a9c1df82fecfea0c6ad6f77fd
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-07-03 08:11:34 +0000

    Enhance JBackgroundPanel class: allow to set relative position to an image in background or stretch it or tile.

---
+++ trunk/app/src/java/org/xblackcat/rojac/gui/component/Fill.java
@@ -0,0 +1,19 @@
+package org.xblackcat.rojac.gui.component;
+
+/**
+ * @author xBlackCat Date: 01.07.11
+ */
+public enum Fill {
+    Center,
+    LeftTop,
+    Left,
+    LeftBottom,
+    Bottom,
+    RightBottom,
+    Right,
+    RightTop,
+    Top,
+    Tile,
+    Stretch,
+    StretchProportional
+}
