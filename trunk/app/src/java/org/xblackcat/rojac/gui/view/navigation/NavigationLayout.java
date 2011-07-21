82ae6773d790c3818907a0fdf143f462f6230b20
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-07-21 14:56:56 +0000

    Prepare to store navigation panel layout

---
+++ trunk/app/src/java/org/xblackcat/rojac/gui/view/navigation/NavigationLayout.java
@@ -0,0 +1,9 @@
+package org.xblackcat.rojac.gui.view.navigation;
+
+import org.xblackcat.rojac.gui.IViewLayout;
+
+/**
+ * @author xBlackCat Date: 21.07.11
+ */
+class NavigationLayout implements IViewLayout {
+}
