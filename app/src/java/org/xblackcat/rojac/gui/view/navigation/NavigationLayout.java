35a883539ce2eb7762a7fe58a6e4f52712fd2cea
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-07-24 11:45:54 +0000

    Store expanded states of first-level nodes in navigation view.

--- app/src/java/org/xblackcat/rojac/gui/view/navigation/NavigationLayout.java
+++ app/src/java/org/xblackcat/rojac/gui/view/navigation/NavigationLayout.java
@@ -6,4 +6,15 @@ import org.xblackcat.rojac.gui.IViewLayout;
  * @author xBlackCat Date: 21.07.11
  */
 class NavigationLayout implements IViewLayout {
+    private static final long serialVersionUID = 1L;
+
+    private final boolean[] expandedStatus;
+
+    public NavigationLayout(boolean[] expandedStatus) {
+        this.expandedStatus = expandedStatus;
+    }
+
+    public boolean[] getExpandedStatus() {
+        return expandedStatus;
+    }
 }
