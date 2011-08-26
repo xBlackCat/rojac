8b8eb45ea6605938c7f528c3adecee6d4c9f92f9
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-08-26 15:04:10 +0000

    More store layouts logic

--- app/src/java/org/xblackcat/rojac/gui/view/thread/ComplexLayout.java
+++ app/src/java/org/xblackcat/rojac/gui/view/thread/ComplexLayout.java
@@ -6,14 +6,16 @@ import org.xblackcat.rojac.gui.IViewLayout;
  * @author xBlackCat
  */
 class ComplexLayout implements IViewLayout {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
     private IViewLayout masterLayout;
     private IViewLayout slaveLayout;
+    private int dividerLocation;
 
-    ComplexLayout(IViewLayout masterLayout, IViewLayout slaveLayout) {
+    ComplexLayout(IViewLayout masterLayout, IViewLayout slaveLayout, int dividerLocation) {
         this.masterLayout = masterLayout;
         this.slaveLayout = slaveLayout;
+        this.dividerLocation = dividerLocation;
     }
 
     public IViewLayout getMasterLayout() {
@@ -22,5 +24,9 @@ class ComplexLayout implements IViewLayout {
 
     public IViewLayout getSlaveLayout() {
         return slaveLayout;
+    }
+
+    public int getDividerLocation() {
+        return dividerLocation;
     }
 }
