ac6ef3b7b395ec7626b9a0d8c91bd4cfac806e37
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-09-02 20:05:18 +0000

    Commit missing file

--- trunk/app/src/java/org/xblackcat/rojac/data/ForumStatistic.java
+++ trunk/app/src/java/org/xblackcat/rojac/data/ForumStatistic.java
@@ -4,35 +4,18 @@ package org.xblackcat.rojac.data;
  * @author xBlackCat
  */
 
-public class ForumStatistic {
+public class ForumStatistic extends ReadStatistic {
     private final int forumId;
-    private final int totalMessages;
-    private final int unreadMessages;
-    private final int unreadReplies;
     private final Long lastMessageDate;
 
     public ForumStatistic(int forumId, int totalMessages, int unreadMessages, Long lastMessageDate, int unreadReplies) {
+        super(unreadReplies, unreadMessages, totalMessages);
         this.forumId = forumId;
-        this.totalMessages = totalMessages;
-        this.unreadMessages = unreadMessages < 0 ? 0 : unreadMessages > totalMessages ? totalMessages : unreadMessages;
         this.lastMessageDate = lastMessageDate;
-        this.unreadReplies = unreadReplies;
-    }
-
-    public int getTotalMessages() {
-        return totalMessages;
-    }
-
-    public int getUnreadMessages() {
-        return unreadMessages;
     }
 
     public Long getLastMessageDate() {
         return lastMessageDate;
-    }
-
-    public int getUnreadReplies() {
-        return unreadReplies;
     }
 
     public int getForumId() {
