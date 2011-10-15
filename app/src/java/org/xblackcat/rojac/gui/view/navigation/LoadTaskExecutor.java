6338f53c3d7694404e54fd199c279dab283d865d
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2011-10-15 19:11:25 +0000

    Use worker to reduce load on EventDispatcher thread

--- app/src/java/org/xblackcat/rojac/gui/view/navigation/LoadTaskExecutor.java
+++ app/src/java/org/xblackcat/rojac/gui/view/navigation/LoadTaskExecutor.java
@@ -1,18 +1,15 @@
 package org.xblackcat.rojac.gui.view.navigation;
 
-import org.xblackcat.rojac.service.ServiceFactory;
-import org.xblackcat.rojac.service.executor.TaskType;
-import org.xblackcat.rojac.service.executor.TaskTypeEnum;
+import org.xblackcat.rojac.util.RojacWorker;
 
-import javax.swing.*;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author xBlackCat
  */
-@TaskType(TaskTypeEnum.DataLoading)
-class LoadTaskExecutor implements Runnable {
+class LoadTaskExecutor extends RojacWorker<Void, LoadTaskExecutor.TaskResult<?>> {
     private final Collection<ALoadTask> tasks = new LinkedList<>();
 
     @SafeVarargs
@@ -22,25 +19,41 @@ class LoadTaskExecutor implements Runnable {
         }
     }
 
-    @SuppressWarnings({"unchecked"})
-    public void run() {
+    public Void perform() {
         for (final ALoadTask task : tasks) {
             try {
                 final Object result = task.doBackground();
 
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        task.doSwing(result);
-                    }
-                });
+                @SuppressWarnings({"unchecked"})
+                TaskResult taskResult = new TaskResult(task, result);
+
+                publish(taskResult);
             } catch (Exception e) {
                 // TODO: do something
             }
         }
+
+        return null;
     }
 
-    public final void execute() {
-        ServiceFactory.getInstance().getExecutor().execute(this);
+    @Override
+    protected void process(List<TaskResult<?>> chunks) {
+        for (TaskResult<?> tr : chunks) {
+            tr.doSwing();
+        }
+    }
+
+    protected static final class TaskResult<T> {
+        final ALoadTask<T> task;
+        final T result;
+
+        private TaskResult(ALoadTask<T> task, T result) {
+            this.result = result;
+            this.task = task;
+        }
+
+        void doSwing() {
+            task.doSwing(result);
+        }
     }
 }
