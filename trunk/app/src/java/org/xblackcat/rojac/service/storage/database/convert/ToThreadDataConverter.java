e5b4cf901f8d53b647cf2a5d1b6531303e9916af
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2012-03-27 17:05:49 +0000

    Fix broken commit

--- trunk/app/src/java/org/xblackcat/rojac/service/storage/database/convert/ToThreadDataConverter.java
+++ trunk/app/src/java/org/xblackcat/rojac/service/storage/database/convert/ToThreadDataConverter.java
@@ -1,6 +1,8 @@
 package org.xblackcat.rojac.service.storage.database.convert;
 
+import org.xblackcat.rojac.data.DiscussionStatistic;
 import org.xblackcat.rojac.data.MessageData;
+import org.xblackcat.rojac.data.ThreadData;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,39 +10,21 @@ import java.sql.SQLException;
 /**
  * @author xBlackCat
  */
-class ToMessageDataConverter implements IToObjectConverter<MessageData> {
+class ToThreadDataConverter implements IToObjectConverter<ThreadData> {
     @Override
-    public MessageData convert(ResultSet rs) throws SQLException {
-        int messageId = rs.getInt(1);
-        int topicId = rs.getInt(2);
-        int parentId = rs.getInt(3);
-        int forumId = rs.getInt(4);
-        int userId = rs.getInt(5);
-        String subject = rs.getString(6);
-        String userName = rs.getString(7);
-        long messageDate = rs.getLong(8);
-        long updateDate = rs.getLong(9);
-        boolean read = rs.getBoolean(10);
-        String rating = rs.getString(11);
-        boolean topicIgnored = rs.getBoolean(12);
-        int parentUserId = rs.getInt(13);
-        boolean userIgnored = rs.getBoolean(14);
+    public ThreadData convert(ResultSet rs) throws SQLException {
+        MessageData messageData = Converters.TO_MESSAGE_DATA.convert(rs);
 
-        return new MessageData(
-                messageId,
-                topicId,
-                parentId,
-                forumId,
-                userId,
-                subject,
-                userName,
-                messageDate,
-                updateDate,
-                read,
-                rating,
-                topicIgnored,
-                parentUserId,
-                userIgnored
-        );
+        int totals = rs.getInt(15);
+        int unreadPosts = rs.getInt(16);
+        int unreadReplies = rs.getInt(17);
+        Long lastPostDate = rs.getLong(18);
+        if (rs.wasNull()) {
+            lastPostDate = null;
+        }
+
+        DiscussionStatistic discussionStatistic = new DiscussionStatistic(totals, unreadPosts, lastPostDate, unreadReplies);
+
+        return new ThreadData(messageData, discussionStatistic);
     }
 }
