5f015bfc6507b06ebef2fc0d3fcda98f15611e48
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2012-04-09 08:42:07 +0000

    Prepare to use IResult interface

---
+++ app/src/java/org/xblackcat/rojac/service/storage/database/helper/AQueryHelper.java
@@ -0,0 +1,50 @@
+package org.xblackcat.rojac.service.storage.database.helper;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.xblackcat.rojac.service.storage.StorageException;
+import org.xblackcat.rojac.service.storage.database.connection.IConnectionFactory;
+import org.xblackcat.rojac.service.storage.database.convert.IToObjectConverter;
+import org.xblackcat.rojac.util.RojacUtils;
+
+import java.util.Iterator;
+
+/**
+ * 09.04.12 11:22
+ *
+ * @author xBlackCat
+ */
+public abstract class AQueryHelper implements IQueryHelper {
+    protected final Log log = LogFactory.getLog(getClass());
+    protected final IConnectionFactory connectionFactory;
+
+    public AQueryHelper(IConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Override
+    public final String getEngine() {
+        return connectionFactory.getEngine();
+    }
+
+    @Override
+    public void shutdown() {
+        connectionFactory.shutdown();
+    }
+
+    @Override
+    public <T> T executeSingle(IToObjectConverter<T> c, String sql, Object... parameters) throws StorageException {
+        assert RojacUtils.checkThread(false, QueryHelper.class);
+
+        Iterable<T> result = execute(c, sql, parameters);
+        Iterator<T> col = result.iterator();
+
+        if (!col.hasNext()) {
+            return null;
+        }
+
+        T object = col.next();
+        assert !col.hasNext() : "Expected one or zero results on query " + RojacUtils.constructDebugSQL(sql, parameters);
+        return object;
+    }
+}
---
+++ app/src/java/org/xblackcat/utils/ReadOnlyIterator.java
@@ -0,0 +1,31 @@
+package org.xblackcat.utils;
+
+import java.util.Iterator;
+
+/**
+* 09.04.12 11:40
+*
+* @author xBlackCat
+*/
+public class ReadOnlyIterator<T> implements Iterator<T> {
+    private final Iterator<T> iterator;
+
+    public ReadOnlyIterator(Iterator<T> iterator) {
+        this.iterator = iterator;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    @Override
+    public T next() {
+        return iterator.next();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("");
+    }
+}
