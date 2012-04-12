6f11ed7ce4e45c16c3a1795ade3210ce700b5955
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2012-04-12 15:00:08 +0000

    Update jCalendar library. Implement clean dialog

--- app/src/java/org/xblackcat/rojac/gui/dialog/options/CleanSettingsDialog.java
+++ app/src/java/org/xblackcat/rojac/gui/dialog/options/CleanSettingsDialog.java
@@ -9,13 +9,19 @@ import org.xblackcat.rojac.service.options.Property;
 import org.xblackcat.rojac.util.WindowsUtils;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 10.04.12 16:56
@@ -24,20 +30,21 @@ import java.util.Locale;
  */
 class CleanSettingsDialog extends JDialog {
     private final JDateChooser chooser = new JDateChooser(new JSpinnerDateEditor());
+    private final Locale locale = Property.ROJAC_GUI_LOCALE.get();
 
     private Long period;
 
     public CleanSettingsDialog(Window owner) {
         super(owner, ModalityType.DOCUMENT_MODAL);
+        setTitle("Clean database");
 
         JPanel cp = new JPanel(new BorderLayout(5, 5));
+        cp.setBorder(new EmptyBorder(10, 10, 10, 10));
         setContentPane(cp);
-
-        Locale locale = Property.ROJAC_GUI_LOCALE.get();
 
         String datePattern = ((SimpleDateFormat) DateFormat.getDateInstance(DateFormat.LONG, locale)).toLocalizedPattern();
 
-        Calendar defTime = Calendar.getInstance(locale);
+        Calendar defTime = makeCalendar(new Date());
         defTime.add(Calendar.MONTH, -3);
 
         chooser.setMaxSelectableDate(new Date());
@@ -45,26 +52,39 @@ class CleanSettingsDialog extends JDialog {
         chooser.setDateFormatString(datePattern);
         chooser.setLocale(locale);
 
-        cp.add(chooser, BorderLayout.NORTH);
+        final SpinnerNumberModel topicsAge = new SpinnerNumberModel(getAge(), 0, 10 * 365, 1);
 
-        Component buttonsBar = WindowsUtils.createButtonsBar(
+        DateTrackListener dateTrackListener = new DateTrackListener(topicsAge);
+        chooser.addPropertyChangeListener("date", dateTrackListener);
+        topicsAge.addChangeListener(dateTrackListener);
+
+        JPanel datePane = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+        datePane.add(new JLabel("Remove forum topics not updated since"));
+        datePane.add(chooser);
+
+        JPanel agePane = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 0));
+        agePane.add(new JLabel("(older than"));
+        agePane.add(new JSpinner(topicsAge));
+        agePane.add(new JLabel("days)"));
+
+        cp.add(
+                WindowsUtils.createColumn(
+                        5,
+                        5,
+                        datePane,
+                        agePane
+                ),
+                BorderLayout.NORTH
+        );
+
+        JComponent buttonsBar = WindowsUtils.createButtonsBar(
                 this,
                 Message.Button_Ok,
                 FlowLayout.CENTER,
                 new AnOkAction() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        period = 0l;
-                        Calendar date = Calendar.getInstance();
-
-                        date.setTime(chooser.getDate());
-
-                        date.clear(Calendar.MILLISECOND);
-                        date.clear(Calendar.SECOND);
-                        date.clear(Calendar.HOUR);
-                        date.clear(Calendar.MINUTE);
-
-                        period = date.getTimeInMillis();
+                        period = chooser.getDate().getTime();
 
                         setVisible(false);
                     }
@@ -79,12 +99,66 @@ class CleanSettingsDialog extends JDialog {
         );
         cp.add(buttonsBar, BorderLayout.SOUTH);
 
-        WindowsUtils.setDialogMinimumWidth(this, buttonsBar);
+        pack();
+        setResizable(false);
         WindowsUtils.center(this);
         setVisible(true);
     }
 
+    private Calendar makeCalendar(Date date) {
+        Calendar cal = Calendar.getInstance(locale);
+
+        cal.setTime(date);
+
+        cal.clear(Calendar.MILLISECOND);
+        cal.clear(Calendar.SECOND);
+        cal.clear(Calendar.MINUTE);
+        cal.clear(Calendar.HOUR_OF_DAY);
+        cal.clear(Calendar.HOUR);
+        cal.clear(Calendar.AM_PM);
+        return cal;
+    }
+
+    private long getAge() {
+        Calendar now = makeCalendar(new Date());
+        Date chosen = chooser.getDate();
+
+        long diff = now.getTimeInMillis() - chosen.getTime();
+
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
     public Long getPeriod() {
         return period;
+    }
+
+    private class DateTrackListener implements PropertyChangeListener, ChangeListener {
+        private final SpinnerNumberModel topicsAge;
+        private boolean changing = false;
+
+        public DateTrackListener(SpinnerNumberModel topicsAge) {
+            this.topicsAge = topicsAge;
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            if (!changing) {
+                changing = true;
+                topicsAge.setValue(getAge());
+                changing = false;
+            }
+        }
+
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            if (!changing) {
+                changing = true;
+                Calendar defTime = makeCalendar(new Date());
+                defTime.add(Calendar.DAY_OF_MONTH, -topicsAge.getNumber().intValue());
+
+                chooser.setDate(defTime.getTime());
+                changing = false;
+            }
+        }
     }
 }
