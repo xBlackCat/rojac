87d90e9196e1046918514f8e6c48fcaffa259025
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2012-04-12 12:42:22 +0000

    Fix translation for "Ignore topics"

--- app/src/java/org/xblackcat/rojac/gui/dialog/ignoreunread/IgnoreTopicsDialog.java
+++ app/src/java/org/xblackcat/rojac/gui/dialog/ignoreunread/IgnoreTopicsDialog.java
@@ -3,11 +3,13 @@ package org.xblackcat.rojac.gui.dialog.ignoreunread;
 import org.jdesktop.swingx.combobox.EnumComboBoxModel;
 import org.xblackcat.rojac.gui.component.ACancelAction;
 import org.xblackcat.rojac.gui.component.AnOkAction;
+import org.xblackcat.rojac.gui.component.DescribableListRenderer;
 import org.xblackcat.rojac.i18n.Message;
 import org.xblackcat.rojac.service.options.Property;
 import org.xblackcat.rojac.util.WindowsUtils;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
@@ -26,20 +28,23 @@ public class IgnoreTopicsDialog extends JDialog {
         super(owner, Message.Dialog_IgnoreTopics_Title.get(), ModalityType.DOCUMENT_MODAL);
 
         setContentPane(createContentPane());
-        
+
         pack();
+        setResizable(false);
     }
 
     private Container createContentPane() {
-        JPanel cp = new JPanel(new BorderLayout());
+        JPanel cp = new JPanel(new BorderLayout(5, 5));
+        cp.setBorder(new EmptyBorder(10, 10, 10, 10));
 
         cp.add(new JLabel(Message.Dialog_IgnoreTopics_TopLine.get()), BorderLayout.NORTH);
 
-        JPanel centerPane = new JPanel(new BorderLayout());
-        
+        JPanel centerPane = new JPanel(new BorderLayout(5, 5));
+
         JComboBox<TopicIgnoringSelection> selectionTypeCB = new JComboBox<>(model);
+        selectionTypeCB.setRenderer(new DescribableListRenderer());
         final JCheckBox showDialogCB = new JCheckBox(Message.Dialog_IgnoreTopics_RememberSettings.get());
-        
+
         showDialogCB.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
