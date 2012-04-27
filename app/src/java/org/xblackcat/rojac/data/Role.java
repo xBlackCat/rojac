b8d0b8c39c5f7be1e7c95287fc1a1797e161cf03
xBlackCat <xBlackCat@04c08baf-c455-0410-824a-7320935b1a7f>
2012-04-27 09:14:52 +0000

    New user role was added to enumeration.

--- app/src/java/org/xblackcat/rojac/data/Role.java
+++ app/src/java/org/xblackcat/rojac/data/Role.java
@@ -1,5 +1,6 @@
 package org.xblackcat.rojac.data;
 
+import org.apache.commons.logging.LogFactory;
 import ru.rsdn.Janus.UserRole;
 
 /**
@@ -12,10 +13,17 @@ public enum Role {
     TeamMember,
     User,
     Expert,
-    Anonym;
+    Anonym,
+    Group;
 
     public static Role getUserType(UserRole ur) {
-        return valueOf(ur.getValue());
+        try {
+            return valueOf(ur.getValue());
+        } catch (IllegalArgumentException e) {
+            LogFactory.getLog(Role.class).warn("Unknown user role " + ur.getValue());
+
+            return User;
+        }
     }
 
     public String toString() {
